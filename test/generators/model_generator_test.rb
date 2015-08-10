278e64f374616f257f711fdca9bf612b94d73227
Sveinn Thorarinsson <sveinn.thorarinsson@appfolio.com>
2015-08-10 11:12:02 -0700

    st - Generator test

---
+++ test/generators/model_generator_test.rb
@@ -0,0 +1,26 @@
+require 'generators/generator_test_helper'
+require 'generators/inheritable_fixtures/model/model_generator'
+
+class ModelGeneratorTest < Rails::Generators::TestCase
+  include GeneratorsTestHelper
+  tests InheritableFixtures::ModelGenerator
+
+  def test_fixtures_path__app_context
+    capture(:stdout) do
+      run_generator %w(Account name:string age:integer)
+    end
+
+    assert_file 'test/fixtures/accounts.yml'
+    assert_no_file 'lib/fixtures/accounts.yml'
+  end
+
+  def test_fixtures_path__engine_context
+    # capture(:stdout) do
+    #   run_generator %w(Account name:string age:integer)
+    # end
+    #
+    # assert_file 'lib/fixtures/accounts.yml'
+    # assert_no_file 'test/fixtures/accounts.yml'
+  end
+
+end
