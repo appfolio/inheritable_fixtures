cc8118c4e101f02170e0e6183675348c7c993da0
Sveinn Thorarinsson <sveinn.thorarinsson@appfolio.com>
2015-08-12 16:49:13 -0700

    st - Test case for generator override in an engine context

--- test/generators/model_generator_test.rb
+++ test/generators/model_generator_test.rb
@@ -1,26 +1,49 @@
 require 'generators/generator_test_helper'
-require 'generators/test_unit/model/model_generator'
+require 'rails/generators/test_unit/model/model_generator'
 
 class ModelGeneratorTest < Rails::Generators::TestCase
   include GeneratorsTestHelper
   tests TestUnit::Generators::ModelGenerator
 
   def test_fixtures_path__app_context
-    capture(:stdout) do
-      run_generator %w(Account name:string age:integer)
-    end
+    run_generator %w(Account name:string age:integer)
 
     assert_file 'test/fixtures/accounts.yml'
     assert_no_file 'lib/fixtures/accounts.yml'
   end
 
   def test_fixtures_path__engine_context
-    # capture(:stdout) do
-    #   run_generator %w(Account name:string age:integer)
-    # end
-    #
-    # assert_file 'lib/fixtures/accounts.yml'
-    # assert_no_file 'test/fixtures/accounts.yml'
+    # To test the model generator override in an engine context we need
+    # to run the generator from the command line.
+
+    in_test_engine_dir do
+      quietly do
+        system('rails g model Account name:string age:integer')
+      end
+
+      assert File.exists?('lib/fixtures/accounts.yml')
+      assert !File.exists?('test/fixtures/accounts.yml')
+    end
+  ensure
+    in_test_engine_dir do
+      quietly do
+        system('rails destroy model Account')
+      end
+    end
+  end
+
+  private
+
+  def quietly
+    capture(:stdout) do
+      yield
+    end
+  end
+
+  def in_test_engine_dir
+    FileUtils.cd('test/test_app/engines/test_engine4') do
+      yield
+    end
   end
 
 end
